73e32e47c8c591bbca2dfc2aea860ba3ef91a7ce
mistlure <mistlure@outlook.com>
2026-04-12 00:41:04 +0200

    Added first controller. Implemented GetAll and Create methods

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,37 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    // ControllerBase gives us access to features like model binding, validation, and HTTP response handling.
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UsersController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var users = await _userRepository.GetAllAsync();
+            return Ok(users);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] User user)
+        {
+            if (user == null) return BadRequest();
+            user.CreatedAt = DateTime.UtcNow;
+            int newId = await _userRepository.AddAsync(user);
+            return Ok(new { id = newId, message = "User created successfully!" });
+        }
+    }
+}
