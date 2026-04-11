e5489f0a34dc82bdf5f7b05cd2c74233dfac61c6
mistlure <mistlure@outlook.com>
2026-04-11 21:46:47 +0200

    Added SQLiteConnectionFactory.cs to enable the use of SQLite

---
+++ Infrastructure/Data/SqliteConnectionFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+
+namespace Infrastructure.Data
+{
+    public class SqliteConnectionFactory : IDbConnectionFactory
+    {
+        private readonly string _connectionString;
+
+        public SqliteConnectionFactory(string connectionString)
+        {
+            _connectionString = connectionString ?? throw new ArgumentNullException(nameof(connectionString));
+        }
+
+        public IDbConnection CreateConnection()
+        {
+            return new SqliteConnection(_connectionString);
+        }
+    }
+}
