9c29b34d33ab8c6a073eb4ab9f17d62558096504
mistlure <mistlure@outlook.com>
2026-05-04 18:27:27 +0200

    Added empty Occupancy and StatusHistory controllers

---
+++ API/Controllers/OccupancyController.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OccupancyController : ControllerBase
+    {
+        private readonly IOccupancyRepository _occupancyRepository;
+
+        public OccupancyController(IOccupancyRepository occupancyRepository)
+        {
+            _occupancyRepository = occupancyRepository;
+        }
+
+
+
+
+    }
+}
---
+++ API/Controllers/StatusHistoryController.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StatusHistoryController : ControllerBase
+    {
+        private readonly IStatusHistoryRepository _statusHistoryRepository;
+
+        public StatusHistoryController(IStatusHistoryRepository statusHistoryRepository)
+        {
+            _statusHistoryRepository = statusHistoryRepository;
+        }
+
+
+
+    }
+}
