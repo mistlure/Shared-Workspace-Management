f09ef67527d7b25aef3fb2a233273b35f635abc5
mistlure <mistlure@outlook.com>
2026-05-04 18:12:21 +0200

    Added WorkplacesController
    - Implemented GetAll, GetById and Create methods

---
+++ API/Controllers/WorkplacesController.cs
@@ -0,0 +1,48 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WorkplacesController : ControllerBase
+    {
+        private readonly IWorkplaceRepository _workplaceRepository;
+
+        public WorkplacesController(IWorkplaceRepository workplaceRepository)
+        {
+            _workplaceRepository = workplaceRepository;
+        }
+
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var workplaces = await _workplaceRepository.GetAllAsync();
+
+            return Ok(workplaces);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var workplace = await _workplaceRepository.GetByIdAsync(id);
+
+            if (workplace == null) return NotFound(new { message = $"Workplace with ID {id} not found." });
+
+            return Ok(workplace);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] Workplace workplace)
+        {
+            if (workplace == null) return BadRequest();
+
+            int newId = await _workplaceRepository.AddAsync(workplace);
+
+            return Ok(new { id = newId, message = "Workplace created successfully!" });
+        }
+    }
+}
